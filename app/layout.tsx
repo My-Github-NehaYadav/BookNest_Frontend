c164d4006e2fd78e8f5cd9350544bca1e05cb00d
Neha Yadav <96839342+My-Github-NehaYadav@users.noreply.github.com>
2026-06-14 19:49:38 +0530

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,39 @@
-import "./globals.css";
-import Link from "next/link";
+// import "./globals.css";
+// import Link from "next/link";
 
-export default function RootLayout({ children }) {
+// export default function RootLayout({ children }) {
+//   return (
+//     <html>
+//       <body>
+//         <nav className="navbar">
+//           <h2>📚 BookNest</h2>
+
+//           <div>
+//             <Link href="/">Home</Link>
+//             <Link href="/add-book">Add Book</Link>
+//           </div>
+//         </nav>
+
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html>
       <body>
-        <nav className="navbar">
-          <h2>📚 BookNest</h2>
-
-          <div>
-            <Link href="/">Home</Link>
-            <Link href="/add-book">Add Book</Link>
-          </div>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/add-book">Add Book</Link>
         </nav>
 
         {children}
